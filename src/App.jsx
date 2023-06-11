5c6f53abe9d9f41401e4841cf2e029f48da88f4a
AdityaSurve <surveaditya521@gmail.com>
2023-06-11 22:26:00 +0530

    colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-7xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-7xl font-bold font-bell underline">Hello world!</h1>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#0b0d17",
+        medium: "#d0d6f9",
+        light: "#ffffff",
+      },
+      fontFamily: {
+        bell: ["Bellefair", "serif"],
+      },
+    },
   },
   plugins: [],
 };
